221f5401ae17ad5defad75f4854c79ad3857eb75
Marina <vaganova.m.a@gmail.com>
2017-11-11 11:58:56 +0500

    first commit: header of index.html

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var run = require("run-sequence");
+
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.scss", ["style"]);
+  gulp.watch("*.html")
+    .on("change", server.reload);
+
+});
+
+gulp.task("build",function(done) {
+  run(
+    "style",
+    done
+  );
+});
